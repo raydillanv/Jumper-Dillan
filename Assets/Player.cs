40639ac35bf3a3beda0171d7616022481ebd7a90
Dillan Victory <113623616+raydillanv@users.noreply.github.com>
2026-05-01 14:23:22 -0500

    added player movement a and d

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 8f;
+    public float jump = 10f; 
+    
+    public AltitudeHandler altitudeHandler;
+
+    private Rigidbody2D rb;
+
+    private bool canJump = false;
+    private bool isDead = false;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDead)
+        {
+            if (Keyboard.current.wKey.wasPressedThisFrame)
+            {
+            
+            } 
+        }
+
+
+
+        float moveInput = 0f; 
+        
+        if (Keyboard.current.aKey.isPressed) moveInput = -1f;
+        if (Keyboard.current.dKey.isPressed) moveInput = 1f;
+        
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+        
         
     }
 }
